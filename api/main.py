4ff9ee93adc24a15d05951ec0202ad2280e4fcc0
khushi-217 <Khushirawatsnd2004@gmail.com>
2026-08-29 17:49:28 +0530

    fix: allow Vite frontend ports in CORS

--- api/main.py
+++ api/main.py
@@ -16,8 +16,8 @@ app.add_middleware(
     "http://localhost:5173",
     "http://localhost:5174",
     "http://localhost:5175",
+    "http://localhost:5176",
 ],
-    
     
     allow_credentials=True,
     allow_methods=["*"],
